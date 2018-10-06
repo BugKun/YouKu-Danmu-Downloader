c301cb1de32d5fd08f22eb81c57f38ed82a5cdb7
BugKun <490964186@qq.com>
2018-10-06 11:50:31 +0800

    init

--- libs/Core.js
+++ libs/Core.js
@@ -17,15 +17,11 @@ module.exports = class {
         return Math.floor(time);
     }
 
-    ramdomUser() {
-        //定义字符串变量colorValue存放可以构成十六进制颜色值的值
-        const randomValue = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f"; //以","为分隔符，将colorValue字符串分割为字符数组["0","1",...,"f"]
+    ramdomHash(len) {
+        const randomValue = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f";
         const randomArray = randomValue.split(",");
-        let random = ""; //定义一个存放十六进制颜色值的字符串变量，先将#存放进去 //使用for循环语句生成剩余的六位十六进制值
-        for (let i = 0; i < 8; i++) {
-            //colorArray[Math.floor(Math.random()*16)]随机取出
-            // 由16个元素组成的colorArray的某一个值，然后将其加在color中，
-            //字符串相加后，得出的仍是字符串
+        let random = "";
+        for (let i = 0; i < len; i++) {
             random += randomArray[Math.floor(Math.random() * 16)];
         }
         return random;
@@ -43,7 +39,7 @@ module.exports = class {
             let ct = 1; //弹幕样式
             let size = 19;  //字体大小
             let content = item.content; //弹幕内容
-            this.xml += `<d p="${ playat },${ ct },${ size },${ color },1430559225,0,${this.ramdomUser()},${this.randomTime()}">${ content }</d>\n`
+            this.xml += `<d p="${ playat },${ ct },${ size },${ color },1430559225,0,${this.ramdomHash(8)},${this.randomTime()}">${ content }</d>\n`
         });
         return true;
     }
